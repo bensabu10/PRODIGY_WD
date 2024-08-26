587296a3713c88d21613e269ea87ba45559271ac
bensabu10 <158448292+bensabu10@users.noreply.github.com>
2024-08-26 12:34:18 +0530

    Create gamelogic.py

---
+++ gamelogic.py
@@ -0,0 +1,19 @@
+def check_winner(board, player):
+    winning_combinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Columns
+        [0, 4, 8], [2, 4, 6]              # Diagonals
+    ]
+    for combo in winning_combinations:
+        if all(board[i] == player for i in combo):
+            return True
+    return False
+
+def make_move(board, move, player):
+    if board[move] == ' ':
+        board[move] = player
+        return True
+    return False
+
+def is_draw(board):
+    return all(cell != ' ' for cell in board)
